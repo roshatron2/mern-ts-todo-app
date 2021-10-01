68607958ee27569c3465aba374cd9d417f868d28
roshatron2 <roshatron2@gmail.com>
2021-10-01 20:18:27 +0530

    removed .env from repo

--- api/src/app.ts
+++ api/src/app.ts
@@ -16,4 +16,4 @@ app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
 
-app.listen(5000, () => console.log("listening on port 5000"));
+app.listen(process.env.PORT || 5000, () => console.log("listening on port 5000"));
